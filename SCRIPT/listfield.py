3e7bbb3a65d916182d87e9fc7af5e08d25b0f328
Chardonnens Bastien <ChardonnensB01@ad.net.fr.ch>
2018-04-12 16:09:29 +0200

    Modification de la description du script listfield.py

--- SCRIPT/listfield.py
+++ SCRIPT/listfield.py
@@ -7,8 +7,7 @@ Created on Mon Jan 29 08:54:33 2018
 
 import arcpy
 
-# For each field in the Hospitals feature class, print
-#  the field name, type, and length.
+# For each field in the feature class print the field's name
 
 fc = [r'C:\Users\ChardonnensB01\AppData\Roaming\ESRI\Desktop10.5\ArcCatalog\SDPCONS.sde\SDPCONS.SDEUSER.SMO1010P_POSTE_COMPTAGE']
 fields = arcpy.ListFields(fc[0])
